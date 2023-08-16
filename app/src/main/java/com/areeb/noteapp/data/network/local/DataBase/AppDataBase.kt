2634294f573bdc53b7e1f19304e2c9aa8851937c
areeb786 <areebrockzz786@gmail.com>
2023-08-16 16:21:31 +0530

    optmized di

--- app/src/main/java/com/areeb/noteapp/data/network/local/DataBase/AppDataBase.kt
+++ app/src/main/java/com/areeb/noteapp/data/network/local/DataBase/AppDataBase.kt
@@ -1,8 +1,6 @@
 package com.areeb.noteapp.data.network.local.DataBase
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.areeb.noteapp.data.models.entitiy.notes.NotesDto
 import com.areeb.noteapp.data.network.local.Dao.NoteDao
@@ -11,26 +9,6 @@ import com.areeb.noteapp.data.network.local.Dao.NoteDao
 abstract class AppDataBase : RoomDatabase() {
     companion object {
         const val VERSION_ONE = 1
-
-        /*
-        * Volatile annotation  is used so that every component in app  get notify that INSTANCE value is changed */
-        @Volatile
-        private var INSTANCE: AppDataBase? = null
-        fun getDatabase(context: Context): AppDataBase {
-            if (INSTANCE == null) {
-                /*
-                * this synchronized block is used to create only instance
-                * */
-                synchronized(this) {
-                    INSTANCE = Room.databaseBuilder(
-                        context.applicationContext,
-                        AppDataBase::class.java,
-                        "noteDB",
-                    ).build()
-                }
-            }
-            return INSTANCE!!
-        }
     }
 
     abstract fun noteDao(): NoteDao
--- app/src/main/java/com/areeb/noteapp/di/ApplicationModule.kt
+++ app/src/main/java/com/areeb/noteapp/di/ApplicationModule.kt
@@ -1,8 +1,10 @@
 package com.areeb.noteapp.di
 
 import android.content.Context
+import androidx.room.Room
 import com.areeb.noteapp.data.network.local.Dao.NoteDao
 import com.areeb.noteapp.data.network.local.DataBase.AppDataBase
+import com.areeb.noteapp.utils.Constants.Companion.NOTE_DB
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -19,7 +21,7 @@ object ApplicationModule {
     fun providesDataBase(
         @ApplicationContext context: Context,
     ): AppDataBase {
-        return AppDataBase.getDatabase(context)
+        return Room.databaseBuilder(context, AppDataBase::class.java, NOTE_DB).build()
     }
 
     @Provides
